52d49ce2ce22e0ab923ebec22c6255290b5c48b6
jacobgates-blip <jacob.gates@rongotai.school.nz>
2026-07-30 10:42:19 +1200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!-- Start of footer 1-->
+<footer class="site-footer pt-5 mt-5">
+    <div class="footer-socials">
+        <a href="#" class="social-icon" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="social-icon" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="social-icon" target="_blank"><i class="fab fa-x-twitter"></i></a>
+        <a href="#" class="social-icon" target="_blank"><i class="fab fa-youtube"></i></a>
+    </div>
+</footer>
+
+<!-- Start of footer 2-->
+<footer class="site-footer  mt-5 text-center">
+    <div class="card-footer text-body-secondary">
+        Your company name
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+</script>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,26 @@
+<!-- Start of Nav bar -->
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about_us.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="menu.php">Our Menu</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
